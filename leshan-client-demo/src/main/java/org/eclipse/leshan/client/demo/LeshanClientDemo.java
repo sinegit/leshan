5de988a2a22f42becf0910969de5f4067c46169c
Shammya Saha <sssaha@sundevils.asu.edu>
2020-12-23 14:32:31 -0700

    Temperature Sensor Added

--- leshan-client-demo/src/main/java/org/eclipse/leshan/client/demo/LeshanClientDemo.java
+++ leshan-client-demo/src/main/java/org/eclipse/leshan/client/demo/LeshanClientDemo.java
@@ -83,58 +83,54 @@ public class LeshanClientDemo {
 
     private static final Logger LOG = LoggerFactory.getLogger(LeshanClientDemo.class);
 
-    // /!\ This field is a COPY of org.eclipse.leshan.server.demo.LeshanServerDemo.modelPaths /!\
+    // /!\ This field is a COPY of
+    // org.eclipse.leshan.server.demo.LeshanServerDemo.modelPaths /!\
     // TODO create a leshan-demo project ?
     public final static String[] modelPaths = new String[] { "LWM2M_LOCKWIPE-v1_0_2.xml",
-                            "LWM2M_Software_Management-v1_0_1.xml", "LWM2M_Cellular_Connectivity-v1_0_1.xml",
-                            "LWM2M_APN_Connection_Profile-v1_0_1.xml", "LWM2M_WLAN_connectivity4-v1_0_1.xml",
-                            "LWM2M_Bearer_Selection-v1_0_1.xml", "LWM2M_Software_Component-v1_0_1.xml",
-                            "LWM2M_DevCapMgmt-v1_0_1.xml", "LWM2M_Portfolio-v1_0_1.xml",
-                            "LwM2M_BinaryAppDataContainer-v1_0_1.xml", "LwM2M_EventLog-v1_0_1.xml",
-                            "LWM2M_VirtualObserveNotify-v1_0.xml",
+            "LWM2M_Software_Management-v1_0_1.xml", "LWM2M_Cellular_Connectivity-v1_0_1.xml",
+            "LWM2M_APN_Connection_Profile-v1_0_1.xml", "LWM2M_WLAN_connectivity4-v1_0_1.xml",
+            "LWM2M_Bearer_Selection-v1_0_1.xml", "LWM2M_Software_Component-v1_0_1.xml", "LWM2M_DevCapMgmt-v1_0_1.xml",
+            "LWM2M_Portfolio-v1_0_1.xml", "LwM2M_BinaryAppDataContainer-v1_0_1.xml", "LwM2M_EventLog-v1_0_1.xml",
+            "LWM2M_VirtualObserveNotify-v1_0.xml",
 
-                            "500.xml", "501.xml",
+            "500.xml", "501.xml",
 
-                            "2048.xml", "2049.xml", "2050.xml", "2051.xml", "2052.xml", "2053.xml", "2054.xml",
-                            "2055.xml", "2056.xml", "2057.xml",
+            "2048.xml", "2049.xml", "2050.xml", "2051.xml", "2052.xml", "2053.xml", "2054.xml", "2055.xml", "2056.xml",
+            "2057.xml",
 
-                            "3200.xml", "3201.xml", "3202.xml", "3203.xml", "3300.xml", "3301.xml", "3302.xml",
-                            "3303.xml", "3304.xml", "3305.xml", "3306.xml", "3308.xml", "3310.xml", "3311.xml",
-                            "3312.xml", "3313.xml", "3314.xml", "3315.xml", "3316.xml", "3317.xml", "3318.xml",
-                            "3319.xml", "3320.xml", "3321.xml", "3322.xml", "3323.xml", "3324.xml", "3325.xml",
-                            "3326.xml", "3327.xml", "3328.xml", "3329.xml", "3330.xml", "3331.xml", "3332.xml",
-                            "3333.xml", "3334.xml", "3335.xml", "3336.xml", "3337.xml", "3338.xml", "3339.xml",
-                            "3340.xml", "3341.xml", "3342.xml", "3343.xml", "3344.xml", "3345.xml", "3346.xml",
-                            "3347.xml", "3348.xml", "3349.xml", "3350.xml", "3351.xml", "3352.xml", "3353.xml",
-                            "3354.xml", "3355.xml", "3356.xml", "3357.xml", "3358.xml", "3359.xml", "3360.xml",
-                            "3361.xml", "3362.xml", "3363.xml", "3364.xml", "3365.xml", "3366.xml", "3367.xml",
-                            "3368.xml", "3369.xml", "3370.xml", "3371.xml", "3372.xml", "3373.xml", "3374.xml",
-                            "3375.xml", "3376.xml", "3377.xml", "3378.xml", "3379.xml", "3380-2_0.xml", "3381.xml",
-                            "3382.xml", "3383.xml", "3384.xml", "3385.xml", "3386.xml", "3387.xml", "3388.xml",
-                            "3389.xml", "3390.xml", "3391.xml", "3392.xml", "3393.xml", "3394.xml", "3395.xml",
-                            "3396.xml", "3397.xml", "3398.xml", "3399.xml", "3400.xml", "3401.xml", "3402.xml",
-                            "3403.xml", "3404.xml", "3405.xml", "3406.xml", "3407.xml", "3408.xml",
+            "3200.xml", "3201.xml", "3202.xml", "3203.xml", "3300.xml", "3301.xml", "3302.xml", "3303.xml", "3304.xml",
+            "3305.xml", "3306.xml", "3308.xml", "3310.xml", "3311.xml", "3312.xml", "3313.xml", "3314.xml", "3315.xml",
+            "3316.xml", "3317.xml", "3318.xml", "3319.xml", "3320.xml", "3321.xml", "3322.xml", "3323.xml", "3324.xml",
+            "3325.xml", "3326.xml", "3327.xml", "3328.xml", "3329.xml", "3330.xml", "3331.xml", "3332.xml", "3333.xml",
+            "3334.xml", "3335.xml", "3336.xml", "3337.xml", "3338.xml", "3339.xml", "3340.xml", "3341.xml", "3342.xml",
+            "3343.xml", "3344.xml", "3345.xml", "3346.xml", "3347.xml", "3348.xml", "3349.xml", "3350.xml", "3351.xml",
+            "3352.xml", "3353.xml", "3354.xml", "3355.xml", "3356.xml", "3357.xml", "3358.xml", "3359.xml", "3360.xml",
+            "3361.xml", "3362.xml", "3363.xml", "3364.xml", "3365.xml", "3366.xml", "3367.xml", "3368.xml", "3369.xml",
+            "3370.xml", "3371.xml", "3372.xml", "3373.xml", "3374.xml", "3375.xml", "3376.xml", "3377.xml", "3378.xml",
+            "3379.xml", "3380-2_0.xml", "3381.xml", "3382.xml", "3383.xml", "3384.xml", "3385.xml", "3386.xml",
+            "3387.xml", "3388.xml", "3389.xml", "3390.xml", "3391.xml", "3392.xml", "3393.xml", "3394.xml", "3395.xml",
+            "3396.xml", "3397.xml", "3398.xml", "3399.xml", "3400.xml", "3401.xml", "3402.xml", "3403.xml", "3404.xml",
+            "3405.xml", "3406.xml", "3407.xml", "3408.xml",
 
-                            "10241.xml", "10242.xml", "10243.xml", "10244.xml", "10245.xml", "10246.xml", "10247.xml",
-                            "10248.xml", "10249.xml", "10250.xml", "10251.xml", "10252.xml", "10253.xml", "10254.xml",
-                            "10255.xml", "10256.xml", "10257.xml", "10258.xml", "10259.xml", "10260-2_0.xml",
-                            "10262.xml", "10263.xml", "10264.xml", "10265.xml", "10266.xml", "10267.xml", "10268.xml",
-                            "10269.xml", "10270.xml", "10271.xml", "10272.xml", "10273.xml", "10274.xml", "10275.xml",
-                            "10276.xml", "10277.xml", "10278.xml", "10279.xml", "10280.xml", "10281.xml", "10282.xml",
-                            "10283.xml", "10284.xml", "10286.xml", "10290.xml", "10291.xml", "10292.xml", "10299.xml",
-                            "10300.xml", "10308-2_0.xml", "10309.xml", "10311.xml", "10313.xml", "10314.xml",
-                            "10315.xml", "10316.xml", "10318.xml", "10319.xml", "10320.xml", "10322.xml", "10323.xml",
-                            "10324.xml", "10326.xml", "10327.xml", "10328.xml", "10329.xml", "10330.xml", "10331.xml",
-                            "10332.xml", "10333.xml", "10334.xml", "10335.xml", "10336.xml", "10337.xml", "10338.xml",
-                            "10339.xml", "10340.xml", "10341.xml", "10342.xml", "10343.xml", "10344.xml", "10345.xml",
-                            "10346.xml", "10347.xml", "10348.xml", "10349.xml", "10350.xml", "10351.xml", "10352.xml",
-                            "10353.xml", "10354.xml", "10355.xml", "10356.xml", "10357.xml", "10358.xml", "10359.xml",
-                            "10360.xml", "10361.xml", "10362.xml", "10363.xml", "10364.xml", "10365.xml", "10366.xml",
-                            "10368.xml", "10369.xml",
+            "10241.xml", "10242.xml", "10243.xml", "10244.xml", "10245.xml", "10246.xml", "10247.xml", "10248.xml",
+            "10249.xml", "10250.xml", "10251.xml", "10252.xml", "10253.xml", "10254.xml", "10255.xml", "10256.xml",
+            "10257.xml", "10258.xml", "10259.xml", "10260-2_0.xml", "10262.xml", "10263.xml", "10264.xml", "10265.xml",
+            "10266.xml", "10267.xml", "10268.xml", "10269.xml", "10270.xml", "10271.xml", "10272.xml", "10273.xml",
+            "10274.xml", "10275.xml", "10276.xml", "10277.xml", "10278.xml", "10279.xml", "10280.xml", "10281.xml",
+            "10282.xml", "10283.xml", "10284.xml", "10286.xml", "10290.xml", "10291.xml", "10292.xml", "10299.xml",
+            "10300.xml", "10308-2_0.xml", "10309.xml", "10311.xml", "10313.xml", "10314.xml", "10315.xml", "10316.xml",
+            "10318.xml", "10319.xml", "10320.xml", "10322.xml", "10323.xml", "10324.xml", "10326.xml", "10327.xml",
+            "10328.xml", "10329.xml", "10330.xml", "10331.xml", "10332.xml", "10333.xml", "10334.xml", "10335.xml",
+            "10336.xml", "10337.xml", "10338.xml", "10339.xml", "10340.xml", "10341.xml", "10342.xml", "10343.xml",
+            "10344.xml", "10345.xml", "10346.xml", "10347.xml", "10348.xml", "10349.xml", "10350.xml", "10351.xml",
+            "10352.xml", "10353.xml", "10354.xml", "10355.xml", "10356.xml", "10357.xml", "10358.xml", "10359.xml",
+            "10360.xml", "10361.xml", "10362.xml", "10363.xml", "10364.xml", "10365.xml", "10366.xml", "10368.xml",
+            "10369.xml",
 
-                            "18830.xml", "18831.xml",};
+            "18830.xml", "18831.xml", "40000.xml"};
 
     private static final int OBJECT_ID_TEMPERATURE_SENSOR = 3303;
+    private static final int OBJECT_ID_NEW_TEMPERATURE_SENSOR = 40000;
     private static final int OBJECT_ID_WATER_FLOW_SENSOR = 34000;
     private static final int OBJECT_ID_WATER_PRESSURE_SENSOR = 33000;
     private final static String DEFAULT_ENDPOINT = "LeshanClientDemo";
@@ -143,8 +139,6 @@ public class LeshanClientDemo {
 
     private static MyLocation locationInstance;
 
-    
-        
     public static void main(final String[] args) throws SQLException {
 
         // Define options for command line tools
@@ -229,12 +223,14 @@ public class LeshanClientDemo {
 
         final StringBuilder trustStoreChapter = new StringBuilder();
         trustStoreChapter.append("\n .");
-        trustStoreChapter.append("\n URI format: file://<path-to-trust-store-file>#<hex-strore-password>#<alias-pattern>");
+        trustStoreChapter
+                .append("\n URI format: file://<path-to-trust-store-file>#<hex-strore-password>#<alias-pattern>");
         trustStoreChapter.append("\n .");
         trustStoreChapter.append("\n Where:");
         trustStoreChapter.append("\n - path-to-trust-store-file is path to pkcs12 trust store file");
         trustStoreChapter.append("\n - hex-store-password is HEX formatted password for store");
-        trustStoreChapter.append("\n - alias-pattern can be used to filter trusted certificates and can also be empty to get all");
+        trustStoreChapter.append(
+                "\n - alias-pattern can be used to filter trusted certificates and can also be empty to get all");
         trustStoreChapter.append("\n .");
         trustStoreChapter.append("\n Default: All certificates are trusted which is only OK for a demo.");
 
@@ -402,7 +398,7 @@ public class LeshanClientDemo {
             if (cl.hasOption("i") || cl.hasOption("cpubk") || cl.hasOption("ccert"))
                 serverURI = "coaps://localhost:" + LwM2m.DEFAULT_COAP_SECURE_PORT;
             else
-                serverURI = "coap://localhost:" + LwM2m.DEFAULT_COAP_PORT;
+                serverURI = "coap://10.203.53.254:" + LwM2m.DEFAULT_COAP_PORT;
         }
 
         // get PSK info
@@ -498,7 +494,8 @@ public class LeshanClientDemo {
 
                 // check input exists
                 if (!input.exists()) {
-                    System.err.println("Failed to load trust store - file or directory does not exist : " + input.toString());
+                    System.err.println(
+                            "Failed to load trust store - file or directory does not exist : " + input.toString());
                     formatter.printHelp(USAGE, options);
                     return;
                 }
@@ -631,12 +628,16 @@ public class LeshanClientDemo {
                 initializer.setInstancesForObject(SERVER, new Server(123, lifetime));
             }
         }
-        // initializer.setInstancesForObject(SECURITY, rpk(serverURI, 123, clientPublicKey.getEncoded(), clientPrivateKey.getEncoded(), serverPublicKey.getEncoded()));
+        // initializer.setInstancesForObject(SECURITY, rpk(serverURI, 123,
+        // clientPublicKey.getEncoded(), clientPrivateKey.getEncoded(),
+        // serverPublicKey.getEncoded()));
         initializer.setInstancesForObject(DEVICE, new MyDevice());
-        initializer.setInstancesForObject(LOCATION, locationInstance);
-        initializer.setInstancesForObject(OBJECT_ID_TEMPERATURE_SENSOR, new RandomTemperatureSensor());
-        initializer.setInstancesForObject(OBJECT_ID_WATER_FLOW_SENSOR, new WaterFlowSensorCSV());
-        initializer.setInstancesForObject(OBJECT_ID_WATER_PRESSURE_SENSOR, new WaterPressureSensor());
+        // initializer.setInstancesForObject(LOCATION, locationInstance);
+        initializer.setInstancesForObject(OBJECT_ID_NEW_TEMPERATURE_SENSOR, new TemperatureSensor());
+        // initializer.setInstancesForObject(OBJECT_ID_WATER_FLOW_SENSOR, new
+        // WaterFlowSensorCSV());
+        // initializer.setInstancesForObject(OBJECT_ID_WATER_PRESSURE_SENSOR, new
+        // WaterPressureSensor());
         List<LwM2mObjectEnabler> enablers = initializer.createAll();
 
         // Create CoAP Config
---
+++ leshan-client-demo/src/main/java/org/eclipse/leshan/client/demo/TemperatureSensor.java
@@ -0,0 +1,171 @@
+package org.eclipse.leshan.client.demo;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.eclipse.leshan.client.resource.BaseInstanceEnabler;
+import org.eclipse.leshan.client.servers.ServerIdentity;
+import org.eclipse.leshan.core.Destroyable;
+import org.eclipse.leshan.core.model.ObjectModel;
+import org.eclipse.leshan.core.response.ExecuteResponse;
+import org.eclipse.leshan.core.response.ReadResponse;
+import org.eclipse.leshan.core.util.NamedThreadFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.re.easymodbus.exceptions.ModbusException;
+import de.re.easymodbus.modbusclient.*;
+
+
+public class TemperatureSensor extends BaseInstanceEnabler implements Destroyable {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TemperatureSensor.class);
+
+    
+    private static final int SENSOR_VALUE = 5700;
+    private static final int SENSOR_VALUE_KELVIN = 6051;
+    private static final int UNITS = 6052;
+    private static final int MAX_MEASURED_VALUE = 5602;
+    private static final int MIN_MEASURED_VALUE = 5601;
+    private static final int RESET_MIN_MAX_MEASURED_VALUES = 5605;
+    private static final List<Integer> supportedResources = Arrays.asList(SENSOR_VALUE, SENSOR_VALUE_KELVIN,UNITS, MAX_MEASURED_VALUE,
+            MIN_MEASURED_VALUE, RESET_MIN_MAX_MEASURED_VALUES);
+    private final ScheduledExecutorService scheduler;
+    private final Random rng = new Random();
+    private double currentTemp = 20d;
+    private double currentTemp_kelvin = 20d;
+    private double minMeasuredValue = currentTemp;
+    private double maxMeasuredValue = currentTemp;
+    private Date timestamp = new Date();
+    private ModbusClient modbusClient = new ModbusClient("127.0.0.1",502);
+
+   
+    public TemperatureSensor() {
+        
+    	try {
+			modbusClient.Connect();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    	
+        this.scheduler = Executors.newSingleThreadScheduledExecutor(new NamedThreadFactory("Temperature Sensor"));
+        scheduler.scheduleAtFixedRate(new Runnable() {
+
+            @Override
+            public void run() {
+                adjustTemperature();
+                adjustKelvinTemperature();
+            }
+        }, 2, 2, TimeUnit.SECONDS);
+    }
+
+    @Override
+    public synchronized ReadResponse read(ServerIdentity identity, int resourceId) {
+        LOG.info("Read on Temperature resource /{}/{}/{}", getModel().id, getId(), resourceId);
+        switch (resourceId) {
+        case MIN_MEASURED_VALUE:
+            LOG.info("Min Measured Value / {}", getTestValue());
+            return ReadResponse.success(resourceId, getTwoDigitValue(minMeasuredValue));
+        case MAX_MEASURED_VALUE:
+            return ReadResponse.success(resourceId, getTwoDigitValue(currentTemp_kelvin));
+        case SENSOR_VALUE:
+            return ReadResponse.success(resourceId, getTwoDigitValue(currentTemp));
+        case SENSOR_VALUE_KELVIN:
+            LOG.info("Sensor Value in KELVIN / {}", getTwoDigitValue(currentTemp_kelvin));
+            return ReadResponse.success(resourceId, getTwoDigitValue(currentTemp_kelvin));
+        case UNITS:
+            return ReadResponse.success(resourceId, getTimeStamp());
+        default:
+            return super.read(identity, resourceId);
+        }
+    }
+    
+    private String getTestValue() 
+    {
+    	
+    	try {
+			return String.valueOf(modbusClient.ReadHoldingRegisters(0, 1)[0]);
+		} catch (ModbusException | IOException e1) {
+			
+			e1.printStackTrace();
+		}
+    	return "";
+    }
+    
+    private String getTimeStamp()
+    {
+        return new Date().toString();
+    }
+
+    @Override
+    public synchronized ExecuteResponse execute(ServerIdentity identity, int resourceId, String params) {
+        LOG.info("Execute on Temperature resource /{}/{}/{}", getModel().id, getId(), resourceId);
+        switch (resourceId) {
+        case RESET_MIN_MAX_MEASURED_VALUES:
+            resetMinMaxMeasuredValues();
+            return ExecuteResponse.success();
+        default:
+            return super.execute(identity, resourceId, params);
+        }
+    }
+
+    private double getTwoDigitValue(double value) {
+        BigDecimal toBeTruncated = BigDecimal.valueOf(value);
+        return toBeTruncated.setScale(2, RoundingMode.HALF_UP).doubleValue();
+    }
+
+    private void adjustTemperature() {
+        float delta = (rng.nextInt(20) - 10) / 10f;
+        currentTemp += delta;
+
+        Integer changedResource = adjustMinMaxMeasuredValue(currentTemp);
+        if (changedResource != null) {
+            fireResourcesChange(SENSOR_VALUE, changedResource);
+        } else {
+            fireResourcesChange(SENSOR_VALUE);
+        }
+    }
+
+    private void adjustKelvinTemperature()
+    {
+        currentTemp_kelvin = currentTemp + 2;
+        fireResourcesChange(SENSOR_VALUE_KELVIN);
+    }
+
+
+    private synchronized Integer adjustMinMaxMeasuredValue(double newTemperature) {
+        if (newTemperature > maxMeasuredValue) {
+            maxMeasuredValue = newTemperature;
+            return MAX_MEASURED_VALUE;
+        } else if (newTemperature < minMeasuredValue) {
+            minMeasuredValue = newTemperature;
+            return MIN_MEASURED_VALUE;
+        } else {
+            return null;
+        }
+    }
+
+    private void resetMinMaxMeasuredValues() {
+        minMeasuredValue = currentTemp;
+        maxMeasuredValue = currentTemp;
+        fireResourcesChange(MIN_MEASURED_VALUE, MAX_MEASURED_VALUE);
+    }
+
+    @Override
+    public List<Integer> getAvailableResourceIds(ObjectModel model) {
+        return supportedResources;
+    }
+
+    @Override
+    public void destroy() {
+        scheduler.shutdown();
+    }
+}
